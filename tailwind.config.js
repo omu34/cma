9c383720309e3d8f1e91d052122f83700ad901d3
bernard omuse <bernardomuse22@gmail.com>
2023-11-09 17:13:31 +0300

    updates made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "dist/*.{html,js,php}"
-    ],
+  content: ["dist/*.{html,js,php}"],
 
-    relative: true,
-    transform: (" (content) => content.replace(/taos:/g,'')"),
-    files: ['dist/*.{php,html,js}'],
+  relative: true,
+  transform: " (content) => content.replace(/taos:/g,'')",
+  files: ["dist/*.{php,html,js}"],
 
-    safelist: [
-        '!duration-[0ms]',
-        '!delay-[0ms]',
-        'html.js :where([class*="taos:"]:not(.taos-init))'
-    ],
+  safelist: [
+    "!duration-[0ms]",
+    "!delay-[0ms]",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
 
-    theme: {
-        screens: {
-            'sm': { 'min': '640px', 'max': '767' },
-            'md': { 'min': '768px', 'max': '1023' },
-            'lg': { 'min': '1024px', 'max': '1279' },
-            'xl': { 'min': '1280px', 'max': '1535' },
-            '2xl': { 'min': '1536px' },
-            'xs': { 'min': '320px', 'max': '567' }
-        },
-        font: {
-            "avenir-bold": ["avenir-bold", "bold"],
-            "avenir-regular": ["avenir-regular", "normal"],
-
-        },
-        extend: {
-            overflow: {
-                hidden: 'hidden',
-            },
-        },
+  theme: {
+    screens: {
+      sm: { min: "640px", max: "767" },
+      md: { min: "768px", max: "1023" },
+      lg: { min: "1024px", max: "1279" },
+      xl: { min: "1280px", max: "1535" },
+      "2xl": { min: "1536px" },
+      xs: { min: "320px", max: "567" },
     },
-    plugins: [
-        require('flowbite/plugin'),
-        require('taos/plugin')
-
-    ],
-}
+    font: {
+      "avenir-bold": ["avenir-bold.Woff", "Serif", "bold"],
+      "avenir-regular": ["avenir-regular.woff", "Serif", "normal"],
+    },
+    extend: {
+      overflow: {
+        hidden: "hidden",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
